2bb58f7164266ec438ba7967ae7fb10f44f9081e
TheKit <nekit1000@gmail.com>
2023-05-10 20:32:47 +0300

    recovery: fix up displayed version
    From "Version . ()" to "Halium 11 (20240413)" for example.

--- recovery.cpp
+++ recovery.cpp
@@ -829,15 +829,15 @@ Device::BuiltinAction start_recovery(Device* device, const std::vector<std::stri
   }
 
   // Extract the YYYYMMDD / YYYYMMDD_HHMMSS timestamp from the full version string.
-  // Assume the first instance of "-[0-9]{8}-", or "-[0-9]{8}_[0-9]{6}-" in case
+  // Assume the first instance of "\.[0-9]{8}\.", or "\.[0-9]{8}_[0-9]{6}\." in case
   // LINEAGE_VERSION_APPEND_TIME_OF_DAY is set to true has the desired date.
-  std::string ver = android::base::GetProperty("ro.lineage.version", "");
+  std::string ver = android::base::GetProperty("ro.build.version.incremental", "");
   std::smatch ver_date_match;
-  std::regex_search(ver, ver_date_match, std::regex("-(\\d{8}(_\\d{6})?)-"));
+  std::regex_search(ver, ver_date_match, std::regex("\\.(\\d{8}(\\.\\d{6})?)\\."));
   std::string ver_date = ver_date_match.str(1);  // Empty if no match.
 
   std::vector<std::string> title_lines = {
-    "Version " + android::base::GetProperty("ro.lineage.build.version", "(unknown)") +
+    "Halium " + android::base::GetProperty("ro.build.version.release", "(unknown)") +
         " (" + ver_date + ")",
   };
   if (android::base::GetBoolProperty("ro.build.ab_update", false)) {
