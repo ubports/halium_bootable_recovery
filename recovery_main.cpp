757bbc7f74284e23c002921e8ab3f61ad31acb68
Jami Kettunen <jami.kettunen@protonmail.com>
2026-03-01 02:38:37 +0200

    (ubports) recovery: Drop default-enabled "Enable ADB" option
    Change-Id: I162baca703ac3b826a50b7b548ee2a3838926424

--- recovery_main.cpp
+++ recovery_main.cpp
@@ -503,9 +503,7 @@ int main(int argc, char** argv) {
     device->RemoveMenuItemForAction(Device::ENTER_RESCUE);
   }
 
-  if (get_build_type() != "userdebug") {
     device->RemoveMenuItemForAction(Device::ENABLE_ADB);
-  }
 
   if (get_build_type() == "user") {
     device->RemoveMenuItemForAction(Device::WIPE_SYSTEM);
